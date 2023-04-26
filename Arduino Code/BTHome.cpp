5c874fa10237d3fd097c78288e3da5373c45f100
countrysideboy <countrysideboy@qq.com>
2023-04-26 05:44:14 +0000

    Update BTHome.cpp

--- Arduino Code/BTHome.cpp
+++ Arduino Code/BTHome.cpp
@@ -2,31 +2,83 @@
 #include "NimBLEDevice.h"
 #include "BTHome.h"
 
-RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
+#include "esp_random.h"
+//#include "mbedtls/ccm.h"
 
-BLEAdvertising *pAdvertising;
+static BLEAdvertising *pAdvertising;
 
-void BTHome::sendPaket(int sensorNumber, int sensor[], int state[], String device_name, bool encryption) {
-
+void BTHome::begin(bool encryption, String enkey) {
   BLEDevice::init("");
-  
-  bootcount++;
-  
-  Serial.printf("start ESP32 %d\n", bootcount);
-
   pAdvertising = BLEDevice::getAdvertising();
+  if (encryption) {
+    this->m_encryptEnable = true;
+    this->m_encryptCount = esp_random() % 0x427;
+  }
+  else this->m_encryptEnable = false;
+}
+
+void BTHome::resetMeasurement() {
+  this->m_payloadIdx = 0;
+}
+
+bool BTHome::addMeasurement_state(uint8_t sensor_id, uint8_t state) {
+  if ((this->m_payloadIdx + 2) <= MEASUREMENT_MAX_LEN) {
+    this->m_payload[this->m_payloadIdx] = static_cast<byte>(sensor_id & 0xff);
+    this->m_payloadIdx++;
+    this->m_payload[this->m_payloadIdx] = static_cast<byte>(state & 0xff);
+    this->m_payloadIdx++;
+    return true;
+  }
+  return false;
+}
+
+bool BTHome::addMeasurement(uint8_t sensor_id, uint64_t value) {
+  uint8_t size = getByteNumber(sensor_id);
+  uint16_t factor = getFactor(sensor_id);
+  if ((this->m_payloadIdx + size + 1) <= MEASUREMENT_MAX_LEN) {
+    this->m_payload[this->m_payloadIdx] = static_cast<byte>(sensor_id & 0xff);
+    this->m_payloadIdx++;
+    for (uint8_t i = 0; i < size; i++)
+    {
+      this->m_payload[this->m_payloadIdx] = static_cast<byte>(((value * factor) >> (8 * i)) & 0xff);
+      this->m_payloadIdx++;
+    }
+    return true;
+  }
+  return false;
+}
+
+bool BTHome::addMeasurement(uint8_t sensor_id, float value) {
+  uint8_t size = getByteNumber(sensor_id);
+  uint16_t factor = getFactor(sensor_id);
+  if ((this->m_payloadIdx + size + 1) <= MEASUREMENT_MAX_LEN) {
+    uint64_t value2 = static_cast<uint64_t>(value * factor);
+    this->m_payload[this->m_payloadIdx] = static_cast<byte>(sensor_id & 0xff);
+    this->m_payloadIdx++;
+    for (uint8_t i = 0; i < size; i++)
+    {
+      this->m_payload[this->m_payloadIdx] = static_cast<byte>((value2 >> (8 * i)) & 0xff);
+      this->m_payloadIdx++;
+    }
+    return true;
+  }
+  return false;
+}
+
+void BTHome::buildPaket(String device_name) {
 
   // Create the BLE Device
 
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
-  BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
+  //BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
 
   oAdvertisementData.setFlags(FLAGS);
-  
+
   std::string strServiceData = "";
   std::string strServiceData2 = "";
-  std::string payload = "";
-  
+  std::string serviceData = "";
+  std::string bthomedata = "";
+
   int dn_length = device_name.length() + 1;
   byte len_buf = dn_length;
   char str_buf[dn_length];
@@ -36,52 +88,38 @@ void BTHome::sendPaket(int sensorNumber, int sensor[], int state[], String devic
   strServiceData += COMPLETE_NAME;   // Complete_Name: Complete local name -- Short_Name: Shortened Name
   strServiceData += str_buf;         // Add the Name to the payload
 
-  payload += SERVICE_DATA;  // Service Data - 16-bit UUID
+  serviceData += SERVICE_DATA;  // DO NOT CHANGE -- Service Data - 16-bit UUID
 
-  payload += UUID1;  // DO NOT CHANGE -- UUID
-  payload += UUID2;  // DO NOT CHANGE -- UUID
+  serviceData += UUID1;  // DO NOT CHANGE -- UUID
+  serviceData += UUID2;  // DO NOT CHANGE -- UUID
 
-  
-  // The encryption is not yet implemented
-  if (encryption){    
-    payload += ENCRYPT;
-    std::string counter = intToFourBytes(bootcount);
-    BLEAddress esp32Mac;  
-    esp32Mac = NimBLEDevice::getAddress();
-    } else {
-      payload += NO_ENCRYPT;
-    }
-
-// Add the sensor data (object id + state) to the Service Data
-  for( int i = 0; i < sensorNumber; i++){
-    
-    byte sensname = static_cast<byte>((sensor[i]) & 0xFF);
-    
-    payload += sensname; 
-
-    if (state[i] != ON && state[i] != OFF){
-      int byteNum = getByteNumber(sensor[i]);
-      Serial.print(i);
-      Serial.printf("\nbyteNum:");
-      Serial.print(byteNum);
-      payload += intToLittleEndianHexString(state[i], byteNum);
-    } else {
-      Serial.print(i);
-      Serial.printf("\nstate:");
-      Serial.print(state[i]);
-      byte st = static_cast<byte>((state[i]) & 0xFF);
-      payload += st;
-    }           
+  // Build the BTHome Data
+  for (int i = 0; i < this->m_payloadIdx; i++)
+  {
+    bthomedata += this->m_payload[i]; // Add the sensor data to the Service Data
   }
-  
-  byte payload_buf = payload.length();    // Generate the length of the Service Data
+
+  // The encryption
+  if (this->m_encryptEnable) {
+    serviceData += ENCRYPT;
+    std::string counter = intToFourBytes(this->m_encryptCount);
+    BLEAddress esp32Mac;
+    esp32Mac = NimBLEDevice::getAddress();
+    this->m_encryptCount++;
+  } else {
+    serviceData += NO_ENCRYPT;
+    serviceData += bthomedata;
+  }
+
+  byte payload_buf = serviceData.length(); // Generate the length of the Service Data
   strServiceData2 += payload_buf;         // Add the length to the Service Data
-  strServiceData2 += payload;             // Finalize the packet
-  
+  strServiceData2 += serviceData;             // Finalize the packet
+
   oAdvertisementData.addData(strServiceData);
   oAdvertisementData.addData(strServiceData2);
   pAdvertising->setAdvertisementData(oAdvertisementData);
-  pAdvertising->setScanResponseData(oScanResponseData);
+  //pAdvertising->setScanResponseData(oScanResponseData);
+  pAdvertising->setScanResponse(false);
   /**  pAdvertising->setAdvertisementType(ADV_TYPE_NONCONN_IND);
        Advertising mode. Can be one of following constants:
      - BLE_GAP_CONN_MODE_NON (non-connectable; 3.C.9.3.2).
@@ -89,24 +127,22 @@ void BTHome::sendPaket(int sensorNumber, int sensor[], int state[], String devic
      - BLE_GAP_CONN_MODE_UND (undirected-connectable; 3.C.9.3.4).
   */
   pAdvertising->setAdvertisementType(BLE_GAP_CONN_MODE_NON);
-  
-  pAdvertising->start();
-  Serial.println("Advertizing started...");
-  delay(100);
+}
+
+void BTHome::stop() {
   pAdvertising->stop();
-  
 }
 
-std::string BTHome::intToLittleEndianHexString(int value, int numBytes) {
-  std::string hexString = "";
-  for (int i = 0; i < numBytes; i++) {
-    byte byteValue = static_cast<byte>((value >> (8 * i)) & 0xFF);
-    hexString += byteValue;
-  }
-  return hexString;
+void BTHome::start(uint32_t duration) {
+  pAdvertising->start(duration);
 }
 
-std::string BTHome::intToFourBytes(int value) {
+bool BTHome::isAdvertising() {
+  return pAdvertising->isAdvertising();
+}
+
+
+std::string BTHome::intToFourBytes(uint32_t value) {
   // Create a buffer to hold the 4 bytes
   uint8_t buffer[4];
 
@@ -119,29 +155,68 @@ std::string BTHome::intToFourBytes(int value) {
   return result;
 }
 
-int BTHome::getByteNumber(int sens) {
-  if ( sens == BATTERY
-        || sens == COUNT
-        || sens == HUMIDITY
-        || sens == MOISTURE
-        || sens == UV ){
+uint8_t BTHome::getByteNumber(uint8_t sens) {
+  if ( sens == ID_BATTERY
+       || sens == ID_COUNT
+       || sens == ID_HUMIDITY
+       || sens == ID_MOISTURE
+       || sens == ID_UV ) {
+    return 1;
+  } else {
+    if ( sens == ID_DURATION
+         || sens == ID_ENERGY
+         || sens == ID_GAS
+         || sens == ID_ILLUMINANCE
+         || sens == ID_POWER
+         || sens == ID_PRESSURE ) {
+      return 3;
+    } else {
+      if ( sens == ID_COUNT4
+           || sens == ID_ENERGY4
+           || sens == ID_GAS4
+           || sens == ID_VOLUME
+           || sens == ID_WATER ) {
+        return 4;
+      } else {
+        return 2;
+      }
+    }
+  }
+}
+
+uint16_t BTHome::getFactor(uint8_t sens) {
+  switch (sens)
+  {
+    case ID_DISTANCEM:
+    case ID_ROTATION:
+    case ID_TEMPERATURE:
+    case ID_VOLTAGE1:
+    case ID_VOLUME1:
+    case ID_UV:
+      return 10; break;
+    case ID_DEWPOINT:
+    case ID_HUMIDITY_PRECISE:
+    case ID_ILLUMINANCE:
+    case ID_MASS:
+    case ID_MASSLB:
+    case ID_MOISTURE_PRECISE:
+    case ID_POWER:
+    case ID_PRESSURE:
+    case ID_SPD:
+    case ID_TEMPERATURE_PRECISE:
+      return 100; break;
+    case ID_CURRENT:
+    case ID_DURATION:
+    case ID_ENERGY:
+    case ID_ENERGY4:
+    case ID_GAS:
+    case ID_GAS4:
+    case ID_VOLTAGE:
+    case ID_VOLUME:
+    case ID_VOLUMEFR:
+    case ID_WATER:
+      return 1000; break;
+    default:
       return 1;
-        } else {
-            if ( sens == DURATION
-                  || sens == ENERGY
-                  || sens == GAS
-                  || sens == ILLUMINANCE
-                  || sens == POWER
-                  || sens == PRESSURE ){
-                return 3;
-                  } else {
-                      if ( sens == COUNT4
-                            || sens == ENERGY4
-                            || sens == GAS4
-                            || sens == VOLUME
-                            || sens == WATER ){                
-                        return 4;
-                            } else { return 2; } 
-                    }
-          }
+  }
 }
