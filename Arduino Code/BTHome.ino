3e9a10cbf62a7768ed746922de7d2570c5ae59cb
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2023-05-13 16:28:07 +0200

    Update BTHome.ino
    Use String for BIND_KEY

--- Arduino Code/BTHome.ino
+++ Arduino Code/BTHome.ino
@@ -11,9 +11,8 @@
 #include "BTHome.h"
 
 #define DEVICE_NAME "DIY-sensor"  // The name of the sensor
-#define ENABLE_ENCRYPT
-uint8_t BIND_KEY[] = {0x23, 0x1d, 0x39, 0xc1, 0xd7, 0xcc, 0x1a, 0xb1, 0xae, 0xe2, 0x24, 0xcd, 0x09, 0x6d, 0xb9, 0x32};
-
+#define ENABLE_ENCRYPT // Remove this line for no encryption
+String BIND_KEY = "231d39c1d7cc1ab1aee224cd096db932"; // Change this key with a string containing 32 of: a-f and 0-9 characters (hex) this will be asked in HA
 
 BTHome bthome;
 
@@ -29,18 +28,18 @@ void setup() {
 void loop() {
   //MEASUREMENT_MAX_LEN = 23, ENABLE_ENCRYPT will use extra 8 bytes, so each Measurement should smaller than 15
   bthome.resetMeasurement();
-  bthome.addMeasurement(ID_ILLUMINANCE, 123.02f);//4 bytes
-  bthome.addMeasurement(ID_PRESSURE, 1001.86f);//4
-  bthome.addMeasurement(ID_TEMPERATURE_PRECISE, 26.00f);//3
-  bthome.addMeasurement(ID_HUMIDITY_PRECISE, 50.00f);//3
+  bthome.addMeasurement(ID_ILLUMINANCE, 1000.02f);//4 bytes
+  bthome.addMeasurement(ID_PRESSURE, 101.86f);//4
+  bthome.addMeasurement(ID_TEMPERATURE_PRECISE, 54.00f);//3
+  bthome.addMeasurement(ID_HUMIDITY_PRECISE, 67.00f);//3
   bthome.buildPaket(DEVICE_NAME);
   bthome.start();  //start the first adv data
   delay(1500);
 
   bthome.resetMeasurement();
-  bthome.addMeasurement_state(STATE_POWER_ON, STATE_ON);//2 bytes
-  bthome.addMeasurement(ID_TVOC, (uint64_t)250);//3
-  bthome.addMeasurement(ID_CO2, (uint64_t)800);//3
+  bthome.addMeasurement_state(STATE_POWER_ON, STATE_OFF);//2 bytes
+  bthome.addMeasurement(ID_TVOC, (uint64_t)213);//3
+  bthome.addMeasurement(ID_CO2, (uint64_t)456);//3
   bthome.buildPaket(DEVICE_NAME);  //change the adv data
   delay(1500);
 
